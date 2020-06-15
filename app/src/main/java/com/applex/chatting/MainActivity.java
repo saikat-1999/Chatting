5df30ba9106a2f7c7f150f17c2dc76cabeb86289
Applex Inc <applex.inc03@gmail.com>
2020-06-15 19:54:57 +0530

    commit

--- app/src/main/java/com/applex/chatting/MainActivity.java
+++ app/src/main/java/com/applex/chatting/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
         }
         if (item.getItemId() == R.id.main_find_friends_option)
         {
-
+            SendUserToFindFriendsActivity();
         }
         return true;
     }
@@ -190,5 +190,11 @@ public class MainActivity extends AppCompatActivity {
         settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(settingsIntent);
         finish();
+    }
+
+    private void SendUserToFindFriendsActivity()
+    {
+        Intent findFriendsIntent = new Intent(MainActivity.this, FindFriendsActivity.class);
+        startActivity(findFriendsIntent);
     }
 }
